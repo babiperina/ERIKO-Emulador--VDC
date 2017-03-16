d737e455503a8a95989c82ccd303acfa6ad23280
Bárbara Perina <M83370@M048167.local>
2017-03-16 17:59:07 -0300

    adição de componentes

--- src/Computador.java
+++ src/Computador.java
@@ -1,12 +1,14 @@
 import componentes.Cpu;
 import componentes.ModuloES;
+import componentes.Parser;
 import componentes.Ram;
 
 public class Computador {
 
-	public static String TAG = "Computador.class";
+	public static final String TAG = "Computador.class";
 
 	public static void main(String[] args) {
+		Parser parser = new Parser();
 		Cpu cpu = new Cpu();
 		Ram ram = new Ram();
 		ModuloES es = new ModuloES();
--- src/componentes/Parser.java
+++ src/componentes/Parser.java
@@ -1,13 +1,59 @@
 package componentes;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import utils.Constantes;
+
 public class Parser {
+
+	public static int instrucaoAtual = -1;
+	public static final String TAG = "Parser.class";
+	public static ArrayList<String> instrucoes = new ArrayList<>();
+
 	
-	public void extractData(){
-		
+	public void sendDataToEncoder() {
+
+
 	}
-	
-	public void sendDataToEncoder(){
-		
+
+	public static void extractData() {
+		File f = new File(Constantes.ARQUIVO_DE_TEXTO);
+
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			fr = new FileReader(f);
+			br = new BufferedReader(fr);
+
+			String linha;
+			while ((linha = br.readLine()) != null) {
+				instrucoes.add(linha);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				fr.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	static {
+		System.out.println(TAG + " criada");
+		extractData();
 	}
 
 }
