288925c3c6c6dc587171cc0a7fe2ffc2226fd26a
MP <myproject.unifor@gmail.com>
2017-03-21 02:46:33 -0300

    construção encoder

--- src/componentes/Encoder.java
+++ src/componentes/Encoder.java
@@ -23,7 +23,8 @@ public class Encoder {
 		Pattern p1 = Pattern.compile(Constantes.RE_add_mov);
 		Pattern p2 = Pattern.compile(Constantes.RE_inc);
 		Pattern p3 = Pattern.compile(Constantes.RE_imul);
-		Matcher type1, type2, type3, matcher;
+		long code[];
+		Matcher type1, type2, type3;
 
 		type1 = p1.matcher(instrucao);
 		type2 = p2.matcher(instrucao);
@@ -40,48 +41,8 @@ public class Encoder {
 				y = type1.group(3);
 
 				if (type.equalsIgnoreCase("mov")) {
-					String msg = "mov ";
-
-					matcher = r.matcher(x);
-					if (matcher.matches()) {
-						// mov register
-						msg += "register, ";
-						matcher = r.matcher(y);
-						if (matcher.matches()) {
-							// mov register, register
-							msg += "register";
-
-						} else {
-							matcher = m.matcher(y);
-							if (matcher.matches()) {
-								// mov register, memory
-								msg += "memory";
-							} else {
-								// mov register, immediate
-								msg += "immediate";
-							}
-						}
-
-					} else {
-						// mov memory
-						msg += "memory, ";
-
-						matcher = r.matcher(y);
-						if (matcher.matches()) {
-							// mov memory, register
-							msg += "register";
-						} else {
-							matcher = m.matcher(y);
-							if (matcher.matches()) {
-								// mov memory, memory
-								msg += "memory";
-							} else {
-								// mov memory, immediate
-								msg += "immediate";
-							}
-						}
-					}
-					System.out.println(msg);
+					code = encoderMovInstruction(r, m, x, y);
+					System.out.println("Instrução codificada em longs: " + code[0] + " " + code[1] + " " + code[2]);
 				} else {
 					System.out.println("add");
 				}
@@ -93,6 +54,72 @@ public class Encoder {
 		} else {
 			System.out.println("Programa encerrado. Error line: " + Computador.parser.instrucaoAtual);
 		}
+	}
+
+	long[] encoderMovInstruction(Pattern r, Pattern m, String x, String y) {
+		long code[] = new long[3];
+		String msg = "mov ";
+		Matcher matcher;
+
+		matcher = r.matcher(x);
+		if (matcher.matches()) {
+			// mov register
+			code[1] = encoderRegister(x);
+			msg += "register, ";
+			matcher = r.matcher(y);
+			if (matcher.matches()) {
+				// mov register, register
+				msg += "register";
+				code[0] = Constantes.VALUE_mov_r_from_r;
+				code[2] = encoderRegister(y);
+			} else {
+				matcher = m.matcher(y);
+				if (matcher.matches()) {
+					// mov register, memory
+					msg += "memory";
+					code[0] = Constantes.VALUE_mov_r_from_m;
+
+				} else {
+					// mov register, immediate
+					msg += "immediate";
+					code[0] = Constantes.VALUE_mov_r_from_i;
+
+				}
+			}
+
+		} else {
+			// mov memory
+			msg += "memory, ";
+
+			matcher = r.matcher(y);
+			if (matcher.matches()) {
+				// mov memory, register
+				msg += "register";
+				code[0] = Constantes.VALUE_mov_m_from_r;
+				code[2] = encoderRegister(y);
+			} else {
+
+				// mov memory, immediate
+				msg += "immediate";
+				code[0] = Constantes.VALUE_mov_m_from_i;
+
+			}
+		}
+		System.out.println(msg);
+		return code;
+	}
+
+	long encoderRegister(String register) {
+		if (register.equalsIgnoreCase("a")) {
+			return Constantes.VALUE_register_A;
+		} else if (register.equalsIgnoreCase("b")) {
+			return Constantes.VALUE_register_B;
+		} else if (register.equalsIgnoreCase("c")) {
+			return Constantes.VALUE_register_C;
+		} else {
+			return Constantes.VALUE_register_D;
+		}
+
 	}
 
 	public void sendInstructionsToESBuffer() {
